60e9b1cc4ac0605cfc2e136c538d85e642e5c206
Alexander Sakhaev <alsakhaev@gmail.com>
2021-03-12 15:45:03 +0300

    test: added boilerplate for testing

---
+++ assembly/__tests__/main.spec.ts
@@ -0,0 +1,11 @@
+// example here:
+// https://github.com/near-examples/guest-book/blob/master/assembly/__tests__/guestbook.spec.ts
+
+import { } from '../main';
+import { VMContext, Context, u128 } from 'near-sdk-as';
+
+describe('test', () => {
+  it('test', () => {
+    expect(true).toBe(true);
+  });
+});
---
+++ src/tests/integration/App-integration.test.js
@@ -0,0 +1,8 @@
+// example here:
+// https://github.com/near-examples/guest-book/blob/master/src/tests/integration/App-integration.test.js
+
+import 'regenerator-runtime/runtime';
+
+it('test', async() => {
+  expect(true).toEqual(true);
+});
