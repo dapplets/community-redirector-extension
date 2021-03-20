5755f5344f98495b7673686141d1c708d407b4dd
Ethernian <dima@ethernian.com>
2021-03-21 01:42:59 +0300

    set different namespaces

--- assembly/model.ts
+++ assembly/model.ts
@@ -6,4 +6,4 @@ export class RedirectInfo {
 }
 
 export const map = new PersistentMap<Uint8Array, RedirectInfo[]>("mm");
-export const keys = new PersistentVector<Uint8Array>("mm");
+export const keys = new PersistentVector<Uint8Array>("m");
